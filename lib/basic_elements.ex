1793421fac55d085d124f0cfe3855a1085c0a92a
Remy Porter <remyporter@jetpackshark.com>
2017-01-07 12:17:59 -0500

    Extended features with convenienc macros.
    `BasicElements` is a module which will contain higher-level abstractions which make building interactive fiction easier. They generate specific kinds of features so that a rich story can be told *without* writing large quantities of Elixir code.

--- lib/basic_elements.ex
+++ lib/basic_elements.ex
@@ -15,4 +15,41 @@ defmodule BasicElements do
             end
         end
     end
+
+    defmacro switch(tag, caption) do
+        quote do
+            switch(unquote(tag), unquote(caption), {"turn it on", "turn it off"})
+        end
+    end
+
+    defmacro switch(tag, caption, {turn_on, turn_off}) do
+        quote do
+            switch(unquote(tag), unquote(caption), {true, false}, 
+                {unquote(turn_on), unquote(turn_off)})
+        end
+    end
+
+    defmacro switch(tag, caption, {on,off}, {turn_on, turn_off}) do
+        quote do
+            feature :switch, unquote(tag), unquote(caption) do
+                Features.setup do
+                    set_state(:state, unquote(off))
+                end
+                action :toggle do
+                    next = case get_state(:state) do
+                        unquote(on) -> unquote(off)
+                        unquote(off) -> unquote(on)
+                    end
+                    set_state(:state, next)
+                    reply(:toggled)
+                end
+                action :examine do
+                    res = case get_state(:state) do
+                        unquote(on) -> unquote(turn_off)
+                        unquote(off) -> unquote(turn_on)
+                    end
+                end
+            end
+        end
+    end
 end
---
+++ test/elements_test.exs
@@ -0,0 +1,22 @@
+defmodule TestElements do
+    use ExUnit.Case
+
+    defmodule Test do
+        use Features
+        use BasicElements
+
+        choice :page52, "Turn to page 52", Test
+
+        switch :lights, "a lamp"
+    end
+
+    test "can make a choice" do
+        {:reply, {:move, Test}, _} = Test.handle_call({:choice, :page52, :choose}, self, %{})
+    end
+
+    test "can flip a switch" do
+        state = Test.init_features(%{})
+        {:reply, :toggled, new_state} = Test.handle_call({:switch, :lights, :toggle}, self, state)
+        %{{:features, :switch, :lights} => %{state: true}} = new_state
+    end
+end
